b7fdbc186290c0a0482e1580014fcd0d67976ca7
Jiwoo Song <jzs6565@E1-055267.ad.psu.edu>
2025-12-27 22:56:53 -0500

    external added

--- __init__.py
+++ __init__.py
@@ -4,6 +4,7 @@ from diffusion_maps.src.krr_model import Modeler
 
 
 root_dir = Path(__file__).resolve().parent
-data_dir = root_dir / "data"
-model_dir = root_dir / "trained_mdls"
+data_dir = str(root_dir / "data")
+model_dir = str(root_dir / "trained_mdls")
+external_dir = str(root_dir / "external")
 
